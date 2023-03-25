14c5824cc0c539f0ab096e0b0f627945842f0dd0
SyamDN <76995702+SyamDN@users.noreply.github.com>
2023-03-25 23:43:19 +0700

    Update Homepage
    Penambahan Card

--- resources/views/Homepage.blade.php
+++ resources/views/Homepage.blade.php
@@ -5,18 +5,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="/Homepage.css" rel="stylesheet" />
-    <link
-            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css"
-            rel="stylesheet"
-            integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD"
-            crossorigin="anonymous"
-        />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-aFq/bzH65dt+w6FI2ooMVUpc+21e0SRygnTpmBvdBgSdnuTN7QbdgL+OapgHtvPp" crossorigin="anonymous">
     <link
             href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
             rel="stylesheet"
         />
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.min.js" integrity="sha384-heAjqF+bCxXpCWLa6Zhcp4fu20XoNIA98ecBC1YkdXhszjoejr5y9Q77hIrv8R9i" crossorigin="anonymous"></script>
     <title>Gative - Game Collective</title>
 </head>
 <body>
@@ -47,7 +42,7 @@
             </a>
             <ul class="dropdown-menu dropdown-menu-dark text-small shadow">
                 <li>
-                    <a class="dropdown-item" href="#">New project...</a>
+                    <a class="dropdown-item" href="#">Message</a>
                 </li>
                 <li><a class="dropdown-item" href="#">Settings</a></li>
                 <li><a class="dropdown-item" href="#">Profile</a></li>
@@ -82,7 +77,120 @@
             </button>
         </div>
         <div class="item p-5">
-            <span class="deals">New Deals</span>
+            <span class="deals mb-5">New Deals</span>
+            <div class="row row-cols-1 row-cols-4 g-4 mt-1">
+                <div class="col">
+                  <div class="card">
+                    <img src="/image/p1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <h6 class="card-subtitle mb-2">Mouse</h6>
+                      <p class="card-text">Rp1.000,00</p>
+                      <button type="button" class="btn px-4">Add to Cart</button>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card">
+                    <img src="/image/p1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <h6 class="card-subtitle mb-2">Mouse</h6>
+                      <p class="card-text">Rp1.000,00</p>
+                      <button type="button" class="btn px-4">Add to Cart</button>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card">
+                    <img src="/image/p1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <h6 class="card-subtitle mb-2">Mouse</h6>
+                      <p class="card-text">Rp1.000,00</p>
+                      <button type="button" class="btn px-4">Add to Cart</button>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card">
+                    <img src="/image/p1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <h6 class="card-subtitle mb-2">Mouse</h6>
+                      <p class="card-text">Rp1.000,00</p>
+                      <button type="button" class="btn px-4">Add to Cart</button>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                  <div class="card">
+                    <img src="/image/p1.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                      <h5 class="card-title">Card title</h5>
+                      <h6 class="card-subtitle mb-2">Mouse</h6>
+                      <p class="card-text">Rp1.000,00</p>
+                      <button type="button" class="btn px-4">Add to Cart</button>
+                    </div>
+                  </div>
+                </div>
+                <div class="col">
+                    <div class="card">
+                      <img src="/image/p1.jpg" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2">Mouse</h6>
+                        <p class="card-text">Rp1.000,00</p>
+                        <button type="button" class="btn px-4">Add to Cart</button>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col">
+                    <div class="card">
+                      <img src="/image/p1.jpg" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2">Mouse</h6>
+                        <p class="card-text">Rp1.000,00</p>
+                        <button type="button" class="btn px-4">Add to Cart</button>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col">
+                    <div class="card">
+                      <img src="/image/p1.jpg" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2">Mouse</h6>
+                        <p class="card-text">Rp1.000,00</p>
+                        <button type="button" class="btn px-4">Add to Cart</button>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col">
+                    <div class="card">
+                      <img src="/image/p1.jpg" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2">Mouse</h6>
+                        <p class="card-text">Rp1.000,00</p>
+                        <button type="button" class="btn px-4">Add to Cart</button>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="col">
+                    <div class="card">
+                      <img src="/image/p1.jpg" class="card-img-top" alt="...">
+                      <div class="card-body">
+                        <h5 class="card-title">Card title</h5>
+                        <h6 class="card-subtitle mb-2">Mouse</h6>
+                        <p class="card-text">Rp1.000,00</p>
+                        <button type="button" class="btn px-4">Add to Cart</button>
+                      </div>
+                    </div>
+                  </div>
+              </div>
+            </div>
         </div>
     @endsection
 </body>
